65bf4474d08f2a878ee6c790ee8f96d267e19376
resqiar <resqiagengr@gmail.com>
2025-03-10 08:04:38 +0700

    update url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 	import Papa from "papaparse";
 
 	// State variables
-	let url = $state("https://585b-111-94-113-192.ngrok-free.app");
+	let url = $state("https://a7dc-111-94-113-192.ngrok-free.app");
 	let path = $state(
 		"/api/chat?passphrase=eJMZBmAL6bNZtMLWHpiQwAksDvIfEaaqt5QZ&config_id=1&app_id=3",
 	);
